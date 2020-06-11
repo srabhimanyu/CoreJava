5871802ac38b8492669217a63e24dd5a2bd1f337
Abhimanyu Srivastava <srabhimanyu@gmail.com>
2020-06-11 12:46:15 +0530

    Add files via upload

---
+++ DeclaringAndInitializing.java
@@ -0,0 +1,64 @@
+package academy.learnprogramming;
+
+import java.io.IOException;
+
+public class DeclaringAndInitializing {
+	
+	
+	public static void main(String[] args)throws IOException{
+		
+		int myNumber; // declaration
+		
+		// System.out.println("myNumber="+myNumber);
+		
+		myNumber=10;	
+		
+		  System.out.println("myNumber="+myNumber); 
+		  
+		  double myDouble=7.50;
+		  
+		  System.out.println("myDouble="+myDouble);
+		  
+		  float myFloat1,myFloat2,myFloat3;
+		  
+		  float myFloat4; float myFloat5;
+		  
+		  boolean b1,b2;
+		  
+		//  double d1, double d2; // doesn't compile
+		  int i1;
+		  int i2;
+		  
+		//  int i3;i4; // doesn't compile
+		  
+		//  int int = 10 ; // doesn't compile
+		
+		
+		  char CHAR;
+		  char Char;
+		  
+		  float okfloat;
+		  double $OKDouble;
+		  
+		  double _alsoDouble;
+		  float ___OKButNotNice$_123;
+		  // illegal example
+		  
+	  //	  double 3point; // erorr
+		//  double my@street; // error
+		  
+		  // only $ or __ is allowed not any other variable
+		  
+	//	  float *$coffee;
+	//	  float double;
+		//  double public;
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+	} // closing of the main method
+} // closing of the class
